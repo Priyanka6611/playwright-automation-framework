56427f730b7d7d182cec430b3bf492b61d00cbab
Priyanka6611 <52905478+Priyanka6611@users.noreply.github.com>
2026-06-10 16:06:27 +1000

    Create LoginTests.cs

---
+++ Tests/LoginTests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace PlaywrightAutomationFramework.Tests
+{
+    public class LoginTests
+    {
+        [Test]
+        public void VerifyLoginPageLoads()
+        {
+            Assert.Pass("Sample Playwright Test");
+        }
+    }
+}
