6817a2a5edd6d46472c34a07276444aad93951f8
Priyanka6611 <52905478+Priyanka6611@users.noreply.github.com>
2026-06-10 16:06:44 +1000

    Create LoginPage.cs

---
+++ Pages/LoginPage.cs
@@ -0,0 +1,9 @@
+namespace PlaywrightAutomationFramework.Pages
+{
+    public class LoginPage
+    {
+        public string UsernameField => "#username";
+        public string PasswordField => "#password";
+        public string LoginButton => "#login";
+    }
+}
